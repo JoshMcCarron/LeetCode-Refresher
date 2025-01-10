f610d3c5bbe438e78d5e6feab5d7ff55a6f1c38c
Josh_McC <joshmcc@my.yorku.ca>
2025-01-10 12:08:26 -0500

    Added java implementation for a stack

---
+++ Stacks/Stack.java
@@ -0,0 +1,25 @@
+
+import java.util.ArrayList;
+public class Stack {
+    ArrayList<Integer> stack = new ArrayList<Integer>();
+    
+    public Stack(){
+
+    }
+
+    public void push(int n){
+        stack.add(n);
+    }
+
+    public int pop(){
+       return  stack.remove(stack.size()-1);
+
+    }
+
+    public int size(){
+        return stack.size();
+    }
+    public int peek(){
+        return stack.get(stack.size()-1);
+    }
+}
