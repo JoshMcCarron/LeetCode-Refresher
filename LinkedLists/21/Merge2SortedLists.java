285de4ecd594de8b7d390552aff586c0ecc0b4a6
Josh_McC <joshmcc@my.yorku.ca>
2025-01-16 17:23:50 -0500

    Completed LeetCode 21

---
+++ LinkedLists/21/Merge2SortedLists.java
@@ -0,0 +1,30 @@
+
+
+public class Merge2SortedLists {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode head = new ListNode(-1);
+        ListNode curr = head;
+
+        while(list1!= null && list2!= null){
+            if (list1.val <= list2.val){
+                curr.next = list1;
+                list1= list1.next;
+            }
+            else{
+                curr.next = list2;
+                list2 = list2.next;
+            }
+            curr = curr.next;
+        }
+        //edge cases
+        if (list1!= null){
+            curr.next=list1;
+        }
+        else{
+            curr.next=list2;
+        }
+
+        //return next and not head because of dummy value
+        return head.next;
+    }
+}
